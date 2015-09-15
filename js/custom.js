6315d57946e79150427887d6ef6614cd9411899b
jonny5v <jonny5v@gmail.com>
2015-09-14 22:19:49 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -52,18 +52,14 @@ $(function() {
     function createSelect(column) {
     	var select = $('<select><option value=""></option></select>')
             .appendTo( $(column.footer()).empty() )
-            .on( 'change', function () {
-                var val = $.fn.dataTable.util.escapeRegex(
-                    $(this).val()
-                );
-
-                column
-                    .search( val ? '^'+val+'$' : '', true, false )
-                    .draw();
+            .on('change', function () {
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search( val ? '^'+val+'$' : '', true, false ).draw();
             });
 
         column.data().unique().sort().each( function ( d, j ) {
             select.append( '<option value="'+d+'">'+d+'</option>' )
         });
+        console.log(select);
     };
 });
