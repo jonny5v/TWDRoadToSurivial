ef66e8f599f6c244d180187cfa2de60c94f87fac
jonny5v <jonny5v@gmail.com>
2015-09-17 14:30:33 -0500

    Update 1.0

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,7 @@
 $(function() {
-	var url = "https://spreadsheets.google.com/feeds/list/1zMDOJlHQLf5z2JVbmsckdIlBRHBKCEG1B0AcKSnk0ZE/od6/public/values?alt=json";
-    
-    $("#table").DataTable({
+    $("#tblCharacter").DataTable({
         ajax: {
-        	url: url,
+        	url: "https://spreadsheets.google.com/feeds/list/1zMDOJlHQLf5z2JVbmsckdIlBRHBKCEG1B0AcKSnk0ZE/1/public/values?alt=json",
             dataSrc: "feed.entry"
         },
         dom: "ftipr",
@@ -13,12 +11,23 @@ $(function() {
                 data: "gsx$charactername.$t"
             },
             {
-                title: "Star",
-                data: "gsx$stars.$t"
+                title: "Stars",
+                data: "gsx$stars.$t",
+                render: function(data, type, full, meta) {
+                    var _stars = "<span style='display:none;'>" + data + "</span>";
+                    for (var i = parseInt(data); i > 0; i-- ) {
+                        _stars += "<i class='glyphicon glyphicon-star'></i>";
+                    }
+                    return _stars;
+                }
             },
             {
                 title: "Trait",
-                data: "gsx$trait.$t"
+                data: "gsx$trait.$t",
+                render: function(data, type, full, meta) {
+                    var _trait = "<span style='display:none;'>" + data + "</span><img src='images/traits/" + data + ".png' width='40' style='display: block; margin: auto;'/>";
+                    return _trait;
+                }
             },
             {
                 title: "Persona",
@@ -44,14 +53,52 @@ $(function() {
         initComplete: function() {
 			this.api().columns().every(function() {
 				var column = this;
-				if (column.index() === 1) {
-					createSelect(column);
-				} else if (column.index() === 2) {
-					createSelect(column);
-				} else if (column.index() === 3) {
-					createSelect(column);
-				}
+				if (column.index() === 1 || column.index() === 2 || column.index() === 3) {
+                    createSelect(column);
+                }
 			});
+        }
+    });
+
+$("#tblWeapons").DataTable({
+        ajax: {
+            url: "https://spreadsheets.google.com/feeds/list/1zMDOJlHQLf5z2JVbmsckdIlBRHBKCEG1B0AcKSnk0ZE/2/public/values?alt=json",
+            dataSrc: "feed.entry"
+        },
+        dom: "ftipr",
+        columns:[
+            {
+                title: "Weapons",
+                data: "gsx$weaponname.$t"
+            },
+            {
+                title: "Stars",
+                data: "gsx$stars.$t"
+            },
+            {
+                title: "Trait",
+                data: "gsx$trait.$t"
+            },
+            {
+                title: "Stats",
+                data: "gsx$stats.$t"
+            }
+        ],
+        columnDefs: [
+            {
+                targets: [1,2,3],
+                orderable: false
+            }
+        ],
+        responsive: true,
+        order: [1, "desc"],
+        initComplete: function() {
+            this.api().columns().every(function() {
+                var column = this;
+                if (column.index() === 1 || column.index() === 2 || column.index() === 3) {
+                    createSelect(column);
+                }
+            });
         }
     });
     
